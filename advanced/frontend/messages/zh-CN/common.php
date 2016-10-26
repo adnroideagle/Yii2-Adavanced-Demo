f2aba823681a05eeaf1371f22dc012c63acd2cc6
mi.rufeng <958936985@qq.com>
2016-10-26 10:58:07 +0800

    add advanced source files .

---
+++ advanced/frontend/messages/zh-CN/common.php
@@ -0,0 +1,16 @@
+<?php
+	
+	/**
+	 * Created by PhpStorm.
+	 * User: tci_mi
+	 * Date: 16/10/26
+	 * Time: 上午10:14
+	 */
+	return [
+		'My Company'=>'崇光科技',
+		'Home'=>'主页',
+		'About'=>'关于',
+		'Contact'=>'联系我们',
+		'Signup'=>'注销',
+		'Login'=>'登录',
+	];
---
+++ advanced/frontend/messages/zh-CN/power.php
@@ -0,0 +1,8 @@
+<?php
+	
+	/**
+	 * Created by PhpStorm.
+	 * User: tci_mi
+	 * Date: 16/10/26
+	 * Time: 上午10:14
+	 */
